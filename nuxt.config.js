6a1e79a5fad0c153f2d6cd46d14b0ca960bc2001
Kananek Thongkam <thkananek@central.co.th>
2018-04-04 18:06:58 +0700

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'RIS EMPLOYEE SERVICES',
+    meta: [
+      { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      { name: 'MobileOptimized', content: 'width' },
+      { name: 'HandheldFriendly', content: 'true' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  css: [
+    './styles/index.scss',
+    'bootstrap-vue/dist/bootstrap-vue.css'
+  ],
+  plugins: [
+    'plugins/bootstrap-vue.js',
+    'plugins/fontawesome.js'
+  ],
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
