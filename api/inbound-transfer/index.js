a59fa22ce59e27aa5002c522b4b03b9fcedc6d1a
Kananek T <info.dvgamer@gmail.com>
2018-08-08 18:08:52 +0700

    update

--- api/inbound-transfer/index.js
+++ api/inbound-transfer/index.js
@@ -1,38 +1,13 @@
 const { Router } = require('express')
-const sql = require('mssql')
-
 const router = Router()
 
-const prod = require('../config-prod.js')
+const dataStatus = require('./ib-status.js')
 /* GET users listing. */
-router.get('/inbound-transfer', (req, res) => {
-  (async () => {
-    let pool = await sql.connect(prod)
-    let results = await pool.request().query(`
-      SELECT COUNT(*) nTotal FROM POSGW..TFileTransUp a WITH(NOLOCK)
-        WHERE a.sStatus IN (3,5)
-      UNION ALL SELECT COUNT(*) FROM POSGW..TFileTransUp a WITH(NOLOCK)
-      WHERE a.sStatus IN (4,6) AND
-        CONVERT(DATETIME,STUFF(STUFF(CONVERT(VARCHAR(8), nStartDate),5,0,'-'),8,0,'-')+' '+STUFF(STUFF(LEFT(RIGHT('000000' + CONVERT(VARCHAR(6), nStartTime), 6),6),3,0,':'),6,0,':'),120)
-        >= CONVERT(DATE, GETDATE())
-      UNION ALL SELECT COUNT(*) FROM POSGW..TFileTransUp a WITH(NOLOCK)
-      WHERE a.sStatus = 0 AND
-        CONVERT(DATETIME,STUFF(STUFF(CONVERT(VARCHAR(8), nStartDate),5,0,'-'),8,0,'-')+' '+STUFF(STUFF(LEFT(RIGHT('000000' + CONVERT(VARCHAR(6), nStartTime), 6),6),3,0,':'),6,0,':'),120)
-        >= CONVERT(DATE, GETDATE())
-    `)
-    if (!!results['recordset']) {
-      res.json({
-        wait: results['recordset'][0].nTotal,
-        fail: results['recordset'][1].nTotal,
-        complete: results['recordset'][2].nTotal
-      })
-    } else {
-      res.json({})
-    }
-    sql.close()
-  })().catch(ex => {
+router.get('/inbound-transfer/status', (req, res) => {
+  dataStatus().then(data => {
+    res.json(data)
+  }).catch(ex => {
     res.json({ error: ex.message.replace(/\\/ig,'\\\"') })
-    sql.close()
   })
 })
 
