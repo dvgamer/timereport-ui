eb0c4a5679f74e5cb7533e192d562235ca7fe530
Kananek Thongkam <info.dvgamer@gmail.com>
2018-12-11 16:19:31 +0700

    fixed data and error global

--- api/data/global-config.js
+++ api/data/global-config.js
@@ -1,5 +1,7 @@
 
 module.exports = [
+  { segment: 'server.reboot_data-sync', field: 'toggle', type: 'bool', created: new Date(), value: true },
+  { segment: 'server.reboot_data-sync', field: 'rebooted', type: 'date', created: new Date(), value: null },
   { segment: 'iis_service_url', field: 'ip48_in', type: 'string', created: new Date(), value: 'http://10.101.147.48/FTPWebService/Service1.asmx' },
   { segment: 'iis_service_url', field: 'ip49_in', type: 'string', created: new Date(), value: 'http://10.101.147.49/FTPWebService/Service1.asmx' },
   { segment: 'iis_service_url', field: 'ip51_in', type: 'string', created: new Date(), value: 'http://10.101.147.51/FTPWebService/Service1.asmx' },
