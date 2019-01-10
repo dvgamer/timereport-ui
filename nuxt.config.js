ee54ee0a94ee8efeb3db4c9a5209113ac4d3fba3
Kananek Thongkam <info.dvgamer@gmail.com>
2019-01-10 16:17:59 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,11 @@ let config = {
   head: {
     titleTemplate: title => (title ? `${title} · ` : '') + 'DevOps'
   },
+  meta: [
+      { name: 'application-name', content: 'DevOps-UI' },
+      { name: 'description', content: pkg.description, id: 'desc' },
+      { name: 'viewport', content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" }
+  ],
   router: {
     middleware: ['auth']
   },
