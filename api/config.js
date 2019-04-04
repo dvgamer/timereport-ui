88591813e46999c5cdb675b6af214f6d904fdc33
Kananek Thongkam <thkananek@central.co.th>
2019-04-04 16:17:52 +0700

    directory change

---
+++ api/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'posdb': {
+    user: 'posquery',
+    password: 'ycmgquer',
+    server: 'posdbrep.cmg.co.th',
+    database: 'POSDB',
+    options: { encrypt: true }
+  },
+  'dev': {
+    user: 'sa',
+    password: 'asdasdasd',
+    server: 'RISHSLTSD3NBZXD',
+    database: 'SURVEY_CMG',
+    options: {}
+  },
+  'prd': {
+    user: 'T1CAdmin',
+    password: 'T!C@dmin',
+    server: 'S-THCW-POSDB95\\CMGSTAGING',
+    database: 'POSDEVOPS_UI',
+    options: { encrypt: true }
+  }
+}
