835f495b187c86633004ef484f4662fbd8876c79
Kananek Thongkam <info.dvgamer@gmail.com>
2019-05-22 11:58:51 +0700

    version package updated and route fixed.

--- api/router/monitor/index.js
+++ api/router/monitor/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/check-hour/:hour', require('./check-hour'))
+router.post('/run-task', require('./run-task'))
 
 // Export the server middleware
 module.exports = router
