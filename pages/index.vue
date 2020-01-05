37003507bfb0a7eaba4971717aa53dfa62320fdc
Kananek Thongkam <info.dvgamer@gmail.com>
2020-01-05 23:32:07 +0700

    Dashboard update.

--- pages/index.vue
+++ pages/index.vue
@@ -9,23 +9,46 @@
         </b-button-group>
       </div>
     </div>
-    <div class="row">
-      <div class="mb-sm-3 col-md-12">
-        <div class="card">
-          <div class="card-body">a</div>
-        </div>
-      </div>
-      <div class="mb-sm-3 col-md-12">
-        <div class="card">
-          <div class="card-body">a</div>
-        </div>
-      </div>
-      <div class="mb-sm-3 col-md-12">
-        <div class="card">
-          <div class="card-body">a</div>
-        </div>
-      </div>
-    </div>
+    <b-row>
+      <b-col md="12" class="mb-2">
+        <b-card>
+          <b-card-title>
+            <h4><fa icon="server" /> Summary Server</h4>
+          </b-card-title>
+          <b-card-body>
+            <ul class="stats">
+              <li class="d-flex">
+                <fa icon="check-circle" class="text-success" />
+                <span>Network and Services</span>
+              </li>
+              <li class="d-flex">
+                <fa icon="times-circle" class="text-danger" />
+                <span>LINE-BOT Heroku</span>
+              </li>
+              <li class="d-flex">
+                <fa icon="minus-circle" class="text-warning" />
+                <span>Database Connection</span>
+              </li>
+            </ul>
+          </b-card-body>
+        </b-card>
+      </b-col>
+      <b-col md="24" class="mb-2">
+        <b-card>
+          <b-card-title>
+            <h4><fa icon="project-diagram" /> POS CMG</h4>
+          </b-card-title>
+          <b-card-body>
+            <b-row class="d-flex panel-item">
+              <b-col>asd</b-col>
+              <b-col>asd</b-col>
+              <b-col>asd</b-col>
+              <b-col>asd</b-col>
+            </b-row>
+          </b-card-body>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script>
@@ -42,5 +65,16 @@ export default {
 </script>
 
 <style scoped>
-
+.stats {
+  margin: 0px;
+}
+.stats li {
+  margin-bottom: 3px;
+}
+.stats li > span {
+  font-weight: bold;
+}
+.stats li > svg {
+  margin: 3px;
+}
 </style>
